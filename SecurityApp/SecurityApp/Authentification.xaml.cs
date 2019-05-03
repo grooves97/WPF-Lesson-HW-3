3c63c353c04cae39d7d94f8a31f318a1766ed890
Aseka <grooves97@gmail.com>
2019-05-03 19:04:16 +0600

    Закончил приложение, дописал класс окна authentification

--- SecurityApp/SecurityApp/Authentification.xaml.cs
+++ SecurityApp/SecurityApp/Authentification.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using SecurityApp.Models;
+using SecurityApp.DataAcces;
+using SecurityApp.Services;
 
 namespace SecurityApp
 {
@@ -22,6 +25,39 @@ namespace SecurityApp
         public Authentification()
         {
             InitializeComponent();
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            User user = new User();
+
+            var newLogin = newLoginTextBox.Text;
+            var newPassword = newPasswordBox.Password;
+            var confirmNewPassword = confirmPasswordBox.Password;
+
+
+            if (string.IsNullOrEmpty(newLogin) || string.IsNullOrEmpty(newPassword) || string.IsNullOrEmpty(confirmNewPassword))
+            {
+                MessageBox.Show("Норм введи");
+                return;
+            }
+
+            if (newPassword != confirmNewPassword)
+            {
+                MessageBox.Show("Пароль должен быть одинаковый Чувак!");
+                return;
+            }
+
+            user.Login = newLogin;
+            user.Password = CryptoService.HashPassword(newPassword);
+
+            using (var context=new SecurityContext())
+            {
+                context.Users.Add(user);
+                context.SaveChanges();
+            }
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
         }
     }
 }
